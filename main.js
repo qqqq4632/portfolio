c3842630505ee4e3f6358c68b909b5cd1b2210da
qqqq4632 <qqqq4632@naver.com>
2020-05-20 22:42:43 +0900

    JS: Home opacity

--- main.js
+++ main.js
@@ -39,6 +39,13 @@ homeContactBtn.addEventListener('click', () => {
  scrollIntoView('#contact');
 });
 
+// Home opacity
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth'});
